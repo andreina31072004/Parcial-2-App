d30e16d21d95649cf8dacac7afa372b6dee2cbf5
andreina31072004 <andreinamilenasc@gmail.com>
2025-05-09 15:45:08 -0500

    .
    .

--- Parcial2App/app/src/main/java/com/example/parcialapp2/MainActivity.java
+++ Parcial2App/app/src/main/java/com/example/parcialapp2/MainActivity.java
@@ -27,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements UsuarioAdaptador.
         listaUsuarios.add(new Usuario("https://i.pinimg.com/736x/34/68/a5/3468a52634ba9421a56148982f92fdf8.jpg", "Andrew Garfield", "Actor", "Spider-Man"));
         listaUsuarios.add(new Usuario("https://m.media-amazon.com/images/I/61g7HDxmyzL._AC_SL1500_.jpg", "Ken Carson", "Artist", "YVL"));
         listaUsuarios.add(new Usuario("https://www.grooveyard.ca/wp-content/uploads/harlowjack-come-home-poster.jpg", "Jack Harlow", "Artista", "Jack"));
+        listaUsuarios.add(new Usuario("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq7ZeDo7Q6nSd88yEelqhEFG96UvQxcbJVPQ&s.jpg", "Drake", "Artista", "Champagne Papi"));
+        listaUsuarios.add(new Usuario("https://media.vogue.es/photos/66bf003863d66e57219a09ec/2:3/w_2560%2Cc_limit/JDR-Or_Crop_Centre_DP-1.jpg", "Rihanna", "Artista", "Riri"));
+        listaUsuarios.add(new Usuario("https://variety.com/wp-content/uploads/2024/09/GettyImages-2001553148.jpg", "Travis Scott", "Artista", "La Flame"));
 
         adaptador = new UsuarioAdaptador(this, listaUsuarios, this);
         rcv_usuarios.setAdapter(adaptador);
